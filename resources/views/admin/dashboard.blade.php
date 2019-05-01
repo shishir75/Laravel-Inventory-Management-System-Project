bfc8384f31401b515ea3a3b221fe727823c8118c
shishir75 <shishir.srdr16@gmail.com>
2019-05-01 13:50:48 +0600

    Dashboard Initialization

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -31,6 +31,168 @@
 		<!-- Main content -->
 		<section class="content">
 			<div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <div class="info-box bg-info">
+                            <span class="info-box-icon"><i class="fa fa-bookmark-o"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Bookmarks</span>
+                                <span class="info-box-number">41,410</span>
+
+                                <div class="progress">
+                                    <div class="progress-bar" style="width: 70%"></div>
+                                </div>
+                                <span class="progress-description">
+                                  70% Increase in 30 Days
+                                </span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <div class="info-box bg-success">
+                            <span class="info-box-icon"><i class="fa fa-thumbs-o-up"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Likes</span>
+                                <span class="info-box-number">41,410</span>
+
+                                <div class="progress">
+                                    <div class="progress-bar" style="width: 70%"></div>
+                                </div>
+                                <span class="progress-description">
+                                  70% Increase in 30 Days
+                                </span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <div class="info-box bg-warning">
+                            <span class="info-box-icon"><i class="fa fa-calendar"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Events</span>
+                                <span class="info-box-number">41,410</span>
+
+                                <div class="progress">
+                                    <div class="progress-bar" style="width: 70%"></div>
+                                </div>
+                                <span class="progress-description">
+                                  70% Increase in 30 Days
+                                </span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <div class="info-box bg-danger">
+                            <span class="info-box-icon"><i class="fa fa-comments-o"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Comments</span>
+                                <span class="info-box-number">41,410</span>
+
+                                <div class="progress">
+                                    <div class="progress-bar" style="width: 70%"></div>
+                                </div>
+                                <span class="progress-description">
+                                  70% Increase in 30 Days
+                                </span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <div class="info-box bg-info">
+                            <span class="info-box-icon"><i class="fa fa-bookmark-o"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Bookmarks</span>
+                                <span class="info-box-number">41,410</span>
+
+                                <div class="progress">
+                                    <div class="progress-bar" style="width: 70%"></div>
+                                </div>
+                                <span class="progress-description">
+                                  70% Increase in 30 Days
+                                </span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <div class="info-box bg-success">
+                            <span class="info-box-icon"><i class="fa fa-thumbs-o-up"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Likes</span>
+                                <span class="info-box-number">41,410</span>
+
+                                <div class="progress">
+                                    <div class="progress-bar" style="width: 70%"></div>
+                                </div>
+                                <span class="progress-description">
+                                  70% Increase in 30 Days
+                                </span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <div class="info-box bg-warning">
+                            <span class="info-box-icon"><i class="fa fa-calendar"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Events</span>
+                                <span class="info-box-number">41,410</span>
+
+                                <div class="progress">
+                                    <div class="progress-bar" style="width: 70%"></div>
+                                </div>
+                                <span class="progress-description">
+                                  70% Increase in 30 Days
+                                </span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <div class="info-box bg-danger">
+                            <span class="info-box-icon"><i class="fa fa-comments-o"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Comments</span>
+                                <span class="info-box-number">41,410</span>
+
+                                <div class="progress">
+                                    <div class="progress-bar" style="width: 70%"></div>
+                                </div>
+                                <span class="progress-description">
+                                  70% Increase in 30 Days
+                                </span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                </div>
 				<div class="row">
 					<div class="col-md-6">
 						<!-- AREA CHART -->
@@ -41,6 +203,21 @@
 							<div class="card-body">
 								<div class="chart">
 									<div id="barchart_material" style="width: 900px; height: 500px;"></div>
+								</div>
+							</div>
+							<!-- /.card-body -->
+						</div>
+						<!-- /.card -->
+					</div>
+					<div class="col-md-6">
+						<!-- AREA CHART -->
+						<div class="card card-primary">
+							<div class="card-header">
+								<h3 class="card-title">Sales Report</h3>
+							</div>
+							<div class="card-body">
+								<div class="chart">
+									<div id="barchart_material2" style="width: 900px; height: 500px;"></div>
 								</div>
 							</div>
 							<!-- /.card-body -->
@@ -90,9 +267,50 @@
 					0: { axis: 'distance' }, // Bind series 0 to an axis named 'distance'.
 					1: { axis: 'brightness' } // Bind series 1 to an axis named 'brightness'.
 				},
+				legend: { position: 'in'},
 			};
 
 			var chart = new google.charts.Bar(document.getElementById('barchart_material'));
+
+			chart.draw(data, google.charts.Bar.convertOptions(options));
+		}
+	</script>
+
+	<script type="text/javascript">
+		google.charts.load('current', {'packages':['bar']});
+		google.charts.setOnLoadCallback(drawChart);
+
+		function drawChart() {
+			var data = google.visualization.arrayToDataTable([
+				['Months', '2019 Sales', '2018 Sales'],
+				['Jan', 1000, 900],
+				['Feb', 1170, 1060],
+				['March', 660, 1120],
+				['May', 1030, 540],
+				['June', 1000, 610],
+				['July', 840, 780],
+				['August', 1210, 1445],
+				['Sept', 1070, 1150],
+				['Oct', 962, 782],
+				['Nov', 750, 440],
+				['Dec', 1358, 1720],
+			]);
+
+			var options = {
+				chart: {
+					title: 'Company Sales Report',
+					subtitle: 'Sales report by months in last two years',
+				},
+				width: 750,
+				bars: 'vertical', // Required for Material Bar Charts.
+				series: {
+					0: { axis: 'distance' }, // Bind series 0 to an axis named 'distance'.
+					1: { axis: 'brightness' } // Bind series 1 to an axis named 'brightness'.
+				},
+				legend: { position: 'left'},
+			};
+
+			var chart = new google.charts.Bar(document.getElementById('barchart_material2'));
 
 			chart.draw(data, google.charts.Bar.convertOptions(options));
 		}
